927fbfee135ca30c3257831eea8683f90354b88d
강세환(플밍4기) <ghks5658@gmail.com>
2026-04-01 16:15:59 +0900

    KillEntity로 플레이어 사망시 카메라 포커스 구현

--- 9TeamHB/Assets/_Game/Scripts/Camera/CameraManager.cs
+++ 9TeamHB/Assets/_Game/Scripts/Camera/CameraManager.cs
@@ -7,6 +7,7 @@ public class CameraManager : MonoBehaviour
     [SerializeField] private StageManager stageManager;
     [SerializeField] private CinemachineCamera vcamP1;
     [SerializeField] private CinemachineCamera vcamP2;
+    [SerializeField] private CinemachineCamera vcamEvent;
     [SerializeField] private BoxCollider2D boundaryCollider;
 
 
@@ -15,6 +16,7 @@ public class CameraManager : MonoBehaviour
         stageManager.Events.StageLoaded += SetFollowTargets;
         stageManager.Events.StageLoaded += SetBoundary;
         stageManager.Events.ActivePlayerChanged += SwitchToPlayer;
+        stageManager.Events.EntityKilled += FocusDeadPlayer;
     }
 
     void OnDisable()
@@ -22,9 +24,10 @@ public class CameraManager : MonoBehaviour
         stageManager.Events.StageLoaded -= SetFollowTargets;
         stageManager.Events.StageLoaded -= SetBoundary;
         stageManager.Events.ActivePlayerChanged -= SwitchToPlayer;
+        stageManager.Events.EntityKilled -= FocusDeadPlayer;
     }
 
-    public void SwitchToPlayer(int playerId)
+    private void SwitchToPlayer(int playerId)
     {
         //Id로 Slot찾기
         StageState state = stageManager.CurrentState;
@@ -34,6 +37,21 @@ public class CameraManager : MonoBehaviour
         // 활성 플래이어를 입력 받으면 카메라 우선 순위 변경
         vcamP1.Priority = (playerNumber == 1) ? 20 : 10;
         vcamP2.Priority = (playerNumber == 2) ? 20 : 10;
+    }
+
+    private void FocusDeadPlayer(int playerId)
+    {
+        //Id로 Slot찾기
+        StageState state = stageManager.CurrentState;
+        state.TryGetEntity(playerId, out EntityState player);
+        if (player.Has<PlayerData>())
+        {
+            int playerNumber = player.Get<PlayerData>().Slot;
+            
+            // 활성 플래이어를 입력 받으면 카메라 우선 순위 변경
+            vcamP1.Priority = (playerNumber == 1) ? 20 : 10;
+            vcamP2.Priority = (playerNumber == 2) ? 20 : 10;
+        }
     }
     private void SetFollowTargets(int stageindex)
     {
@@ -60,17 +78,21 @@ public class CameraManager : MonoBehaviour
 
     private void SetFollowTargets(CinemachineCamera vcam, GridEntityView view)
     {
-        vcam.Target.TrackingTarget = view.transform;
+        vcam.Follow = view.transform;
         vcam.ForceCameraPosition( view.transform.position, vcam.transform.rotation );
+    }
+
+    private void ShowEventCamera(GridEntityView view)
+    {
+        SetFollowTargets(vcamEvent, view);
+        vcamEvent.Priority = 100;
     }
 
     private void SetBoundary(int stageindex)
     {
         // 콜라이더 크기 변경
-        float width = stageManager.CurrentState.Width + 2f; 
-        if(width < 17f) width = 17f;
         float height = stageManager.CurrentState.Height + 2f ;
-        if(height < 10f) height = 10f;
+        float width = stageManager.CurrentState.Width + 2f; 
         boundaryCollider.size = new Vector2(width, height);
         
         // 콜라이더 오프셋
@@ -79,10 +101,32 @@ public class CameraManager : MonoBehaviour
         boundaryCollider.offset = new Vector2(offsetX, offsetY);
         boundaryCollider.transform.position = new Vector2(-1.5f, 1.5f);
         
+        // 최소 콜라이더 크기 확보
+        float camW = Camera.main.orthographicSize * Camera.main.aspect * 2f;
+        float camH = Camera.main.orthographicSize * 2f;
+        if( camW < width ) camW = width;
+        if( camH < height ) camH = height;
+        boundaryCollider.size = new Vector2(width, height);
+        
+        
         // 컨파이너 캐시 업데이트
         var confiner = vcamP1.GetComponent<CinemachineConfiner2D>();
         confiner.InvalidateBoundingShapeCache();
         confiner = vcamP2.GetComponent<CinemachineConfiner2D>();
         confiner.InvalidateBoundingShapeCache();
+    }
+
+    
+    
+    
+    
+    public bool IsPointVisible(Vector3 worldPos)
+    {
+        // 월드 좌표를 뷰포트 좌표(0~1 사이)로 변환
+        Vector3 viewPos = Camera.main.WorldToViewportPoint(worldPos);
+    
+        // 0~1 사이라면 화면 안
+        return viewPos.x >= 0 && viewPos.x <= 1 && 
+               viewPos.y >= 0 && viewPos.y <= 1 && viewPos.z > 0;
     }
 }
