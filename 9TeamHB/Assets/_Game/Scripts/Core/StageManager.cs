44992386775cd7432e75af2a49c26d389ba92c60
Kyeongmin Kim <kkfenix3085@naver.com>
2026-04-02 17:43:27 +0900

    Fix Snapshot Timing

--- 9TeamHB/Assets/_Game/Scripts/Core/StageManager.cs
+++ 9TeamHB/Assets/_Game/Scripts/Core/StageManager.cs
@@ -170,6 +170,8 @@ namespace MyGame2.Stage
 
         public TurnOutcome TryExecuteTurn(Direction direction)
         {
+            StageSnapshot snapshot = new StageSnapshot(CurrentState);
+
             if (!CanAcceptInput())
             {
                 LastOutcome = TurnOutcome.Ignored(MoveResult.Blocked(
@@ -182,7 +184,6 @@ namespace MyGame2.Stage
 
             if (outcome.Executed)
             {
-                StageSnapshot snapshot = new StageSnapshot(CurrentState);
                 snapshotStack.Push(snapshot);
 
                 Debug.Log($"Push Snapshot into Stack, Stack Size : {snapshotStack.Count}");
